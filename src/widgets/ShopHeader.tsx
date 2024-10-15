506514f79518a2ecaf270e059da23fa4ba5e6229
Aleksey <viqwer3@yandex.ru>
2024-10-16 00:54:55 +0500

    refactor: create balance component

--- src/widgets/ShopHeader.tsx
+++ src/widgets/ShopHeader.tsx
@@ -1,21 +1,13 @@
 import { Link } from 'react-router-dom'
-import { CoinIcon } from '../icons/CoinIcon'
 import { QuestionIcon } from '../icons/QuestionIcon'
 import { TeamIcon } from '../icons/TeamIcon'
-import { TetherIcon } from '../icons/TetherIcon'
+import Balance from "../components/Balance.tsx";
 
 export const ShopHeader = () => {
 	return (
 		<div className='flex items-center justify-between'>
 			<div className='flex items-center justify-center gap-[10px]'>
-				<div className='shadow-inset-custom rounded-full pl-[3px] pr-[10px] flex items-center gap-[6px]'>
-					<CoinIcon />
-					<div className='font-medium text-[20px]'>321</div>
-				</div>
-				<div className='shadow-inset-custom rounded-full pl-[3px] pr-[10px] flex items-center gap-[6px]'>
-					<TetherIcon />
-					<div className='font-medium text-[20px]'>321</div>
-				</div>
+				<Balance/>
 				<div className='shadow-inset-custom rounded-full pl-[3px] pr-[10px] flex items-center gap-[6px]'>
 					<TeamIcon />
 					<div className='font-medium text-[20px]'>321</div>
